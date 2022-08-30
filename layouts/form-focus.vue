772d7d1e2f84d4919abaf622d79fbd8f209c5d85
chaldrak <chaldrakus@gmail.com>
2022-08-30 16:14:03 +0100

    project cloned and adding of the environment variables

--- layouts/form-focus.vue
+++ layouts/form-focus.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="md:p-10 p-5">
-    <div class="card m-auto w-96 bg-base-100 shadow-xl">
+  <div class="p-5 md:p-10">
+    <div class="m-auto shadow-xl card w-96 bg-base-100">
       <div class="card-body">
         <slot></slot>
       </div>
